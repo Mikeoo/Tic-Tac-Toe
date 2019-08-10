a32d1b40023f15c494bdf2fd19156be5ca9db153
Mike Overbeek <mikeoverbeek@outlook.com>
2019-08-10 13:35:37 +0200

    more ui tweaks

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace WpfApp1
 {
@@ -20,9 +10,32 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region private variables
+        /// <summary>
+        /// Holds the current value of the cells
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True if player1 turn(x) false if player2 turn (0)
+        /// </summary>
+        private bool mPlayer1turn;
+        #endregion
+        #region default constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            newGame();
+        }
+        #endregion default constructor defa
+
+        private void newGame()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ WpfApp1/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace WpfApp1
+{
+    /// <summary>
+    /// The type of value the grid hold
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// the cell not clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// the cell is nought
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// the cell is cross
+        /// </summary>
+        Cross
+    }
+}
